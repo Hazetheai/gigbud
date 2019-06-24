0bb7972d6e740f8b2f0f54211eef0bbbaf142826
Jake <jacobstefan92@gmail.com>
2019-06-23 22:59:21 -0400

    implemented reactroutes on nav

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,37 @@
 import React from "react";
-
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import Navigation from "../Navigation/Navigation";
 import { Normalize } from "@smooth-ui/core-em";
 import { Button } from "@smooth-ui/core-em";
 import Landing from "../Landing/Landing";
 import Home from "../Home/Home";
+import SignUp from "../SignUp/SignUp";
+import SignIn from "../SignIn/SignIn";
+import PasswordForgetPage from "../PasswordForget/PasswordForget";
+import AccountPage from "../Account/Account";
+import AdminPage from "../Admin/Admin";
+
+import * as ROUTES from "../constants/routes";
 
 const signedIn = false;
 
 const App = () => (
-  <div>
-    <>
-      <Normalize />
-    </>
-    <h1>Gigbud</h1>
-    <Button variant="dark">Sign out</Button>
-    {signedIn === true ? <Home /> : <Landing />}
-  </div>
+  <Router>
+    <div>
+      <>
+        <Normalize />
+      </>
+      <Navigation />
+      <Link>Sign out</Link>
+      <Route exact path={ROUTES.LANDING} component={Landing} />
+      <Route path={ROUTES.SIGN_UP} component={SignUp} />
+      <Route path={ROUTES.SIGN_IN} component={SignIn} />
+      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+      <Route path={ROUTES.HOME} component={Home} />
+      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+      <Route path={ROUTES.ADMIN} component={AdminPage} />
+    </div>
+  </Router>
 );
 
 export default App;
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,8 +1,73 @@
+/**@jsx jsx */
+
 import React from "react";
+import { jsx, css } from "@emotion/core";
+import { Normalize } from "@smooth-ui/core-em";
+import * as ROUTES from "../constants/routes";
+import { Link } from "react-router-dom";
+
+const listItem = css`
+  padding: 10px;
+`;
+
+const listLink = css`
+  textdecoration: none;
+  color: black;
+`;
 
 const Navigation = () => (
   <div>
-    <h1>Navigation</h1>
+    <>
+      <Normalize />
+    </>
+    <nav
+      css={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "space-between",
+        backgroundColor: "hotpink",
+        alignItems: "baseline"
+      }}
+    >
+      {" "}
+      <h1>Gigbud</h1>
+      <div>
+        <ul
+          css={{
+            listStyle: "none",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "baseline"
+          }}
+        >
+          <li css={listItem}>
+            <Link css={listLink} to={ROUTES.SIGN_IN}>
+              Sign In
+            </Link>
+          </li>
+          <li css={listItem}>
+            <Link css={listLink} to={ROUTES.LANDING}>
+              Landing
+            </Link>
+          </li>
+          <li css={listItem}>
+            <Link css={listLink} to={ROUTES.HOME}>
+              Home
+            </Link>
+          </li>
+          <li css={listItem}>
+            <Link css={listLink} to={ROUTES.ACCOUNT}>
+              Account
+            </Link>
+          </li>
+          <li css={listItem}>
+            <Link css={listLink} to={ROUTES.ADMIN}>
+              Admin
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </div>
 );
 
